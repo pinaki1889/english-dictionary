4bf7b60af1c1ed44bdacec535eafb9563b62558f
pinaki <bppit.11500219053@gmail.com>
2021-05-21 21:43:19 +0530

    The first time

---
+++ dic_web/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DicWebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dic_web'
---
+++ dic_web/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from PyDictionary import PyDictionary
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+def word(request):
+    if request.method == 'GET':
+        search = request.GET.get('search')
+        dictionary = PyDictionary()
+        meaning = dictionary.meaning(search)
+        synonyms = dictionary.synonym(search)
+        antonyms = dictionary.antonym(search)
+        context = {
+            'meaning': meaning,
+            #['Noun'][0],
+            'synonyms': synonyms,
+            'antonyms': antonyms
+        }
+        if(context['meaning'] == None):
+            return render(request, 'error.html')
+        else:
+            return render(request, 'word.html', context)
+
